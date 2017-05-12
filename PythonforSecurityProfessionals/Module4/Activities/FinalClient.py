dca2a8e57560b9602806953a5017fd6f86bf50ef
Sim Ben <sbenoit10@gmail.com>
2017-05-12 10:44:12 -0400

    ftp client activity.

--- PythonforSecurityProfessionals/Module4/Activities/FinalClient.py
+++ PythonforSecurityProfessionals/Module4/Activities/FinalClient.py
@@ -26,24 +26,98 @@ import os, re, socket, threading, struct, sys
 from ctypes import *
 
 def read_file(filename): #ctypes
-    return
+    kernel32 = windll.LoadLibrary("kernel32")
+    file_handle = kernel32.CreateFileA(filename, 0x10000000, 0, 0, 3, 0x80, 0)
+    if (file_handle == -1):
+        return -1
+    data = create_string_buffer(4096)
+    read_data = c_int(0)
+    bool_success = kernel32.ReadFile(file_handle, byref(data), 4096, byref(read_data), 0)
+    kernel32.CloseHandle(file_handle)
+    if bool_success == 0:
+        return -1
+    return data.value
     
 def create_file(filename, data): #ctypes
+    kernel32 = windll.LoadLibrary("kernel32")
+    file_handle = kernel32.CreateFileA(filename, 0x10000000, 0, 0, 2, 0x80, 0)
+    if (file_handle == -1):
+        return -1
+    written_data = c_int(0)
+    bool_success = kernel32.WriteFile(file_handle, data, len(data), byref(written_data), 0)
+    kernel32.CloseHandle(file_handle)
+    if bool_success == 0:
+        return -1
     return
 
 def recv_data(sock): #Implement a networking protocol
-    return
+    data_len = struct.unpack("!I", sock.recv(4))
+    return sock.recv(data_len)
     
 def send_data(sock,data): #Implement a networking protocol
+    data_len = len(data)
+    sock.send(struct.pack("!I", data_len))
+    sock.send(data)
     return
     
-def send_file_contents(file_name,usersock): #DOWNLOAD
+def send_file_contents(file_name,user_sock): #DOWNLOAD
+    if send_data(user_sock, read_file(file_name)) == -1:
+        send_data(user_sock, "File sending failed")
     return
 
-def receive_file_contents(file_name,usersock):#UPLOAD
-    return 
+def receive_file_contents(file_name,user_sock):#UPLOAD
+    if create_file(file_name, recv_data(user_sock)) == -1:
+        send_data(user_sock, "File creation failed.")
+    return
     
 def main():
+    command_list = ["DRIVESEARCH",
+                    "DIRSEARCH",
+                    "DOWNLOAD",
+                    "UPLOAD",
+                    "CLOSE"]
+    continue_bool = True
+
+    if len(sys.argv) < 2:
+        print "Usage %s [ADDRESS]" % sys.argv[0]
+        exit()
+
+    sock =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock.connect((sys.argv[1], 55555))
+    except:
+        print "Cannot connect to %s" % sys.argv[1]
+
+    while(continue_bool != False):
+        command = raw_input(read_file(sock)).upper()
+        send_data(sock, command)
+        if command not in command_list:
+            print recv_data(sock)
+            print command_list
+
+        elif command == "DRIVESEARCH":
+            file_name = raw_input(recv_data(sock))
+            send_data(sock, file_name)
+            print recv_data()
+
+        elif command == "DIRSEARCH":
+            file_name = raw_input(recv_data(sock))
+            send_data(sock, file_name)
+            print recv_data()
+
+        elif command == "DOWNLOAD":
+            file_name = raw_input(recv_data(sock))
+            send_data(sock, file_name)
+            receive_file_contents(raw_input("Local filename: "), sock)
+
+        elif command == "UPLOAD":
+            file_name = raw_input(recv_data(sock))
+            new_file_name = raw_input("File to create: ")
+            send_data(sock, new_file_name)
+            send_file_contents(file_name, sock)
+            
+        elif command == "CLOSE":
+            continue_bool = False
     return
             
 main()
--- PythonforSecurityProfessionals/Module4/Activities/FinalServer.py
+++ PythonforSecurityProfessionals/Module4/Activities/FinalServer.py
@@ -22,37 +22,123 @@ This activity will require you to use multithreading, ctypes, regular
 expressions, and some libraries with which you're unfamiliar. ENJOY!
 '''
 
-import os, re, socket, threading, struct
+import os, re, threading, struct, sys, socket
 from ctypes import *
 
 def read_file(filename): #ctypes
-    return
+	kernel32 = windll.LoadLibrary("kernel32")
+	file_handle = kernel32.CreateFileA(filename, 0x10000000, 0, 0, 3, 0x80, 0)
+	if (file_handle == -1):
+		return -1
+	data = create_string_buffer(4096)
+	read_data = c_int(0)
+	bool_success = kernel32.ReadFile(file_handle, byref(data), 4096, byref(read_data), 0)
+	kernel32.CloseHandle(file_handle)
+	if bool_success == 0:
+		return -1
+	return data.value
 
 def create_file(filename, data): #ctypes
-    return
+	kernel32 = windll.LoadLibrary("kernel32")
+	file_handle = kernel32.CreateFileA(filename, 0x10000000, 0, 0, 2, 0x80, 0)
+	if (file_handle == -1):
+		return -1
+	written_data = c_int(0)
+	bool_success = kernel32.WriteFile(file_handle, data, len(data), byref(written_data), 0)
+	kernel32.CloseHandle(file_handle)
+	if bool_success == 0:
+		return -1
+	return
 
-def recv_data(sock): #Implement a networking protocol
-    return
+def recv_data(user_sock): #Implement a networking protocol
+	data_len = struct.unpack("!I", user_sock.recv(4))
+	return user_sock.recv(data_len)
 
-def send_data(sock,data): #Implement a networking protocol
-    return
+def send_data(user_sock,data): #Implement a networking protocol
+	data_len = len(data)
+	user_sock.send(struct.pack("!I", data_len))
+	user_sock.send(data)
+	return
 
 def search_drive(file_name): #DRIVESEARCH
-    return
+	re_obj = re.compile(file_name)
+	for root, dirs, files in os.walk("C:\\"):
+		for file in files:
+			if (re.search(re_obj, file))
+				return os.path.join(root, file)
+	return -1
 
 def search_directory(file_name): #DIRSEARCH
-    return
+    root, dirs, files = os.walk(os.getcwd())
+    re_obj = re.compile(file_name)
+    for file in files:
+		if (re.search(re_obj, file)):
+			return os.path.join(root, file)
+    return -1
 
-def send_file_contents(file_name,usersock,userinfo): #DOWNLOAD
-    return
+def send_file_contents(file_name,user_sock,userinfo): #DOWNLOAD
+	if send_data(user_sock, read_file(file_name)) == -1:
+		send_data(user_sock, "File sending failed")
+	return
 
-def receive_file_contents(file_name,usersock):#UPLOAD
-    return
+def receive_file_contents(file_name,user_sock):#UPLOAD
+	if create_file(file_name, recv_data(user_sock)) == -1:
+		send_data(user_sock, "File creation failed.")
+	return
 
-def handle_connection(usersock,userinfo):
-    return
+def handle_connection(user_sock,userinfo):
+	command_list = ["DRIVESEARCH",
+					"DIRSEARCH",
+					"DOWNLOAD",
+					"UPLOAD",
+					"CLOSE"]
+	continue_bool = True
+	while(continue_bool):
+		command = recv_data(user_sock).upper()
 
+		if (command == "DRIVESEARCH"):
+			send_data(user_sock, "Filename: ")
+			search_result = search_drive(recv_data(user_sock))
+			if search_result == -1:
+				send_data(user_sock, "File not found.")
+			else:
+				send_data(user_sock, search_result)
+
+		if (command == "DIRSEARCH"):
+			send_data(user_sock, "Filename: ")
+			search_result = search_drive(recv_data(user_sock))
+			if search_result == -1:
+				send_data(user_sock, "File not found.")
+			else:
+				send_data(user_sock, search_result)
+
+		if (command == "DOWNLOAD"):
+			send_data(user_sock, "Filename: ")
+			search_drive(recv_data(user_sock), user_sock, userinfo)
+
+		if (command == "UPLOAD"):
+			send_data(user_sock, "Filename: ")
+			search_drive(recv_data(user_sock), user_sock)
+
+		if (command == "CLOSE"):
+			continue_bool = False
+			return
+		else:
+			send_data(user_sock, "INVALID COMMAND")
 def main():
-    return
+
+	try:
+		server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		server_socket.bind(('',55555))
+	except socket.error, msg:
+		sys.stderr.write("[ERROR] %s\n" % msg[1])
+		sys.exit(1)
+
+	while(True):
+		server_socket.listen(8)
+		user_sock, userinfo = server_socket.accept()
+		conn_thread = threading.Thread(None, handle_connection, None, (user_sock, userinfo))
+		conn_thread.start()
+	return
 
 main()
